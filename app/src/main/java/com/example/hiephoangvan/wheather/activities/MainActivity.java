87473e6b73e1247223dc0690d4fb8ea867280047
hiep.hoangvan <hoanghiep1097@gmail.com>
2018-11-08 17:25:46 +0700

    nav and tab

---
+++ app/src/main/java/com/example/hiephoangvan/wheather/activities/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.hiephoangvan.wheather.activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.example.hiephoangvan.wheather.R;
+import com.example.hiephoangvan.wheather.adapters.ViewpagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerlayout;
+    @BindView(R.id.navigationView)
+    NavigationView mNavigationView;
+    @BindView(R.id.content_layout)
+    FrameLayout mContentLayout;
+    @BindView(R.id.tabLayout)
+    TabLayout mTabLayout;
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    FragmentManager mFragmentManager;
+    ViewpagerAdapter mViewpagerAdapter;
+    ActionBarDrawerToggle mDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mFragmentManager = getSupportFragmentManager();
+        mViewpagerAdapter = new ViewpagerAdapter(mFragmentManager);
+        mViewPager.setAdapter(mViewpagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+        setSupportActionBar(mToolbar);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+        mDrawerToggle.syncState();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerlayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/hiephoangvan/wheather/adapters/ViewpagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.hiephoangvan.wheather.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.hiephoangvan.wheather.fragments.FragmentHourly;
+import com.example.hiephoangvan.wheather.fragments.FragmentToday;
+
+public class ViewpagerAdapter extends FragmentStatePagerAdapter {
+    public ViewpagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new FragmentToday();
+                break;
+            case 1:
+                fragment = new FragmentHourly();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title = "Today";
+                break;
+            case 1:
+                title = "Hourly";
+                break;
+        }
+        return title;
+    }
+}
